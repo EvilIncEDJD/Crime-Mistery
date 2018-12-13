e0ec80ec3a28f1a7613c07e16e74fc1e36eecc40
Unknown <carlos-.-alves@hotmail.com>
2018-12-13 15:55:13 +0000

    interacao com o Policia, audio...

---
+++ CrimeAndMystery/Assets/OpenInformacao.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenInformacao : MonoBehaviour {
+
+	public GameObject policeUI;
+	public static bool policeUIOpen = false;
+	
+	// Use this for initialization
+	void Start () {
+		policeUI.SetActive(false);
+	}
+	
+	void OnTriggerStay(Collider other){
+		policeUI.SetActive(true);
+		
+	}
+	void OnTriggerExit(){
+		policeUI.SetActive(false);
+	}
+}
